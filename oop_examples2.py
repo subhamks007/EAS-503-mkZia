2fe8920e19c40ed343c59986d1ede33ce756786e
Mohammad K. Zia <mkzia@buffalo.edu>
2019-09-18 21:59:53 -0400

    Update

--- oop_examples2.py
+++ oop_examples2.py
@@ -3,6 +3,12 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
 
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Student(Person):
     def __init__(self, first_name, last_name, credit_hours, q_point):
