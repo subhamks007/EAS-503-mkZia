7fb55765ee55af423496a393fdd5a7219b1cf123
Mohammad K. Zia <mkhawarzia@gmail.com>
2020-02-05 18:56:37 -0500

    Update

--- week2/function_exercises_solution.py
+++ week2/function_exercises_solution.py
@@ -56,3 +56,14 @@ def calculate_distance(x1,y1,x2,y2):
     distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
     return distance
+
+# 8 ---------------------------
+def coin_toss()
+    import random
+
+    number = random.randint(0, 1)
+
+    if number == 0:
+        return 'Head'
+    else:
+        return 'Tail'
