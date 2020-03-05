acc0139f16c4ec7b1847b2539a7f24f48d1f57fc
Mohammad K. Zia <mkhawarzia@gmail.com>
2020-03-04 21:51:22 -0500

    Update

--- week6/oop_examples.py
+++ week6/oop_examples.py
@@ -1,8 +1,8 @@
-# class Person:
-#     def __init__(self, first_name, last_name, email):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.email = email
+class Person:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
 
 
 # class Person:
